22e2f77d43fb5ebca9c9569eae6a2113867a075c
Anna <wierrdo@yandex.ru>
2020-10-20 17:55:09 +0300

    Update database config

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -10,10 +10,41 @@
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
 
- const findPublicRole = async () => {
+const findPublicRole = async () => {
   const result = await strapi
     .query("role", "users-permissions")
     .findOne({ type: "public" });
   return result;
 };
-module.exports = () => {};
+
+const setDefaultPermissions = async () => {
+  const role = await findPublicRole();
+  const permissions = await strapi
+    .query("permission", "users-permissions")
+    .find({ type: "application", role: role.id });
+  await Promise.all(
+    permissions.map(p =>
+      strapi
+        .query("permission", "users-permissions")
+        .update({ id: p.id }, { enabled: true })
+    )
+  );
+};
+
+const isFirstRun = async () => {
+  const pluginStore = strapi.store({
+    environment: strapi.config.environment,
+    type: "type",
+    name: "setup"
+  });
+  const initHasRun = await pluginStore.get({ key: "initHasRun" });
+  await pluginStore.set({ key: "initHasRun", value: true });
+  return !initHasRun;
+};
+
+module.exports = () => {
+     const shouldSetDefaultPermissions = await isFirstRun();
+  if (shouldSetDefaultPermissions) {
+    await setDefaultPermissions();
+  }
+};
