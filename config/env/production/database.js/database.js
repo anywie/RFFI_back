a978d6b86d1e2eb583fd2c9e9f2bd30717536ce7
Anna <wierrdo@yandex.ru>
2020-10-21 11:59:54 +0300

    make it better

--- config/env/production/database.js/database.js
+++ config/env/production/database.js/database.js
@@ -20,3 +20,4 @@ module.exports = ({ env }) => ({
     },
   },
 });
+ postgres://ibmdhxdobdsryu:ea8f673aef543b079d6f41a1909b79dd0de67e4005ff00597b0179b4f621d3c0@ec2-34-237-236-32.compute-1.amazonaws.com:5432/d3ddv4gej9ol3k
