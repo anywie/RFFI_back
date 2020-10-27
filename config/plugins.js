cac3fc9827b31c1945a95dd564855fb3192d7393
Anna <wierrdo@yandex.ru>
2020-10-27 17:34:43 +0300

    aws-s3

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,24 @@
+// module.exports = ({ env }) => ({
+//   upload: {
+//     provider: "cloudinary",
+//     providerOptions: {
+//       cloud_name: env("CLOUDINARY_NAME"),
+//       api_key: env("CLOUDINARY_KEY"),
+//       api_secret: env("CLOUDINARY_SECRET"),
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   upload: {
-    provider: "cloudinary",
+    provider: "aws-s3",
     providerOptions: {
-      cloud_name: env("CLOUDINARY_NAME"),
-      api_key: env("CLOUDINARY_KEY"),
-      api_secret: env("CLOUDINARY_SECRET"),
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      region: "us-west-1",
+      params: {
+        Bucket: "myystrapi",
+      },
     },
   },
 });
